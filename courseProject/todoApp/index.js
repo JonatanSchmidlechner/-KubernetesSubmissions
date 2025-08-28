fcf5750d4a2c720e10212bf854a7746910d4b4d3
Jonatan <jonatan.schmidlechner@tuni.fi>
2025-08-28 18:56:01 +0300

    Fix deployment.yaml. I forgot to build new image.

--- courseProject/todoApp/index.js
+++ courseProject/todoApp/index.js
@@ -5,16 +5,14 @@ import { fileURLToPath } from 'url';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Fix __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve a file safely
 app.get('/', (req, res) => {
-  // Adjust the path to the file you really want to serve
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
+console.log(__dirname);
+console.log(path.join(__dirname, 'index.html'));
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
