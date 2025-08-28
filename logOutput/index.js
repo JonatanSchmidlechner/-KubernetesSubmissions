c9b73767399f7578bc0f08581ff1ac5d7a0142cf
Jonatan <jonatan.schmidlechner@tuni.fi>
2025-08-28 18:33:53 +0300

    Add express to logoutput

--- logOutput/index.js
+++ logOutput/index.js
@@ -1,9 +1,21 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 const randomString = Math.random().toString(36);
 
 const printRandomStringIntervally = () => {
-    console.log(`${new Date().toISOString()}: ${randomString}`);
+  console.log(`${new Date().toISOString()}: ${randomString}`);
 
-    setTimeout(printRandomStringIntervally, 5000);
-}
+  setTimeout(printRandomStringIntervally, 5000);
+};
 printRandomStringIntervally();
