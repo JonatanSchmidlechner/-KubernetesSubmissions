2744622392a7ef14c1f413bc355be925064df7d6
Jonatan <jonatan.schmidlechner@tuni.fi>
2025-09-05 18:43:39 +0300

    Fix env variables.

--- logOutput-pingpong/logOutput/index.js
+++ logOutput-pingpong/logOutput/index.js
@@ -3,16 +3,17 @@ import fetch from 'node-fetch';
 import fs from 'fs/promises';
 const app = express();
 const port = process.env.PORT || 3000;
-const pingURL = process.env.PING_URL | `http://localhost:3001/pings`;
-const infoFilePath = process.env.INFO_FILE || './config/information.txt';
+const pingURL = process.env.PING_URL || `http://localhost:3001/pings`;
+const infoFilePath = process.env.INFO_FILE_PATH || './config/information.txt';
 const randomString = Math.random().toString(36);
 
 app.get('/', async (req, res) => {
+  console.log('info file path:', infoFilePath);
   const response = await fetch(pingURL);
   const data = await response.json();
   let fileContent = '';
   try {
-    fileContent = fs.readFile(infoFilePath, 'utf-8');
+    fileContent = await fs.readFile(infoFilePath, 'utf-8');
   } catch (error) {
     console.log(error);
     fileContent = 'Error: Could not read file content';
