05e9f017e264f18448a702bcee97d531450e614e
Jonatan <jonatan.schmidlechner@tuni.fi>
2025-09-05 18:55:37 +0300

    Remove unused URL.

--- courseProject/todoApp/routes/rootRouter.js
+++ courseProject/todoApp/routes/rootRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import fetch from "node-fetch"
+import fetch from 'node-fetch';
 import { handleImageState } from '../services/imageManager.js';
 
 const rootRouter = express.Router();
-const todoBackendBaseURL = process.env.TODO_BACKEND_URL || "http://localhost:3002";
-const todoAppBaseURL = process.env.TODO_APP_URL || "http://localhost:3000"
-const externalTodoAppBaseURL = process.env.EXTERNAL_TODO_APP_URL || "http://localhost:3000";
+const todoBackendBaseURL =
+  process.env.TODO_BACKEND_URL || 'http://localhost:3002';
+const externalTodoAppBaseURL =
+  process.env.EXTERNAL_TODO_APP_URL || 'http://localhost:3000';
 
 rootRouter.get('/', async (req, res) => {
   await handleImageState();
@@ -16,25 +17,28 @@ rootRouter.get('/', async (req, res) => {
   res.render('index', {
     filePath: '/images/image.png',
     todos: todos,
-    todoAppBaseURL: externalTodoAppBaseURL
+    todoAppBaseURL: externalTodoAppBaseURL,
   });
 });
 
-rootRouter.post("/todos", async (req, res) => {
+rootRouter.post('/todos', async (req, res) => {
   const data = req.body;
   console.log(data);
   try {
-    const response = await fetch(`${todoBackendBaseURL}/todos`, {method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data)})
+    const response = await fetch(`${todoBackendBaseURL}/todos`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
     if (!response.ok) {
-      res.status(response.status).send("Failed to create todo");
+      res.status(response.status).send('Failed to create todo');
     }
 
-    res.redirect("/")
+    res.redirect('/');
   } catch (error) {
-    console.log("Internal server error", error)
-    res.status(500).send("Internal server error")
+    console.log('Internal server error', error);
+    res.status(500).send('Internal server error');
   }
-
-})
+});
 
 export default rootRouter;
