d6a170f04e890b8b187ba1252635360910eb6561
Jonatan <jonatan.schmidlechner@tuni.fi>
2025-09-24 15:22:52 +0300

    Fix nats url

--- courseProject/todo-backend/nats/index.js
+++ courseProject/todo-backend/nats/index.js
@@ -3,11 +3,11 @@ const initNats = async () => {
   while (true) {
     try {
       const nc = await connect({
-        servers: [process.env.NATS_URL || 'nats://nats:4222'],
+        servers: [process.env.NATS_URL || 'nats://my-nats:4222'],
       });
       return nc;
     } catch (err) {
-      console.log('Nats not ready, retrying in 5s...');
+      console.log('Nats not ready, retrying in 5s...', err);
       await new Promise((r) => setTimeout(r, 5000));
     }
   }
--- courseProject/todo-broadcaster/src/server.js
+++ courseProject/todo-broadcaster/src/server.js
@@ -4,7 +4,7 @@ const initNats = async () => {
   while (true) {
     try {
       const nc = await connect({
-        servers: [process.env.NATS_URL || 'nats://nats:4222'],
+        servers: [process.env.NATS_URL || 'nats://my-nats:4222'],
       });
       return nc;
     } catch (err) {
